84212af415baeb20aadaab1b2c55328d57fdd943
Nikhil Kumar Mishra <nkmishra1997@gmail.com>
2017-01-02 18:40:09 +0530

    Changed subplot 4

---
+++ erode_filter.m
@@ -0,0 +1,25 @@
+function [count,fingers]=erode_filter(a2,width,ratio)
+erode_filter=ones(1,floor(width/ratio));
+a1=imclose(a2,strel('disk', 5,5));
+a3=imfill(a1,'holes');
+eroded_image1=imerode(a3,erode_filter);
+eroded_image2=imerode(eroded_image1,strel('disk', 3,4));
+eroded_image=bwareaopen(eroded_image2,40);
+palm=imdilate(eroded_image,erode_filter);
+finger=~palm.*a2; %figure;imshow(finger)
+finger1=imopen(finger,ones(10,2));
+%figure;imshow(finger1);
+x3=imerode(finger1,ones(3,3));
+fingers=bwareaopen(x3,500);%figure;imshow(fingers);
+%figure;imshow(fingers);title('fingers');
+%x3=imclearborder(fingers);figure;imshow(x3);
+%x2=bwconncomp(fingers,8);
+%number_of_fingers=x2.NumObjects;
+%%
+stats = regionprops(fingers);
+count=0;
+for index=1:length(stats)
+    if  (stats(index).BoundingBox(3)*stats(index).BoundingBox(4) > 100);
+      count=count+1;
+     end
+end
--- num_finger_30_12.m
+++ num_finger_30_12.m
@@ -1,14 +1,17 @@
 function [number]=num_finger_30_12(image,rmean,bmean,rbcov)
  [a2,width]= FINGERS_30_12(image,rmean,bmean,rbcov);
- [number_of_fingers1] = erode_filter(a2,width,4);
- [number_of_fingers2] = erode_filter(a2,width,3);
+ [number_of_fingers1,fingers1] = erode_filter(a2,width,4);
+ [number_of_fingers2,fingers2] = erode_filter(a2,width,3);
 %%jk
 number=0;
 if number_of_fingers1==number_of_fingers2;
     number=number_of_fingers1;
+    subplot(2,2,4);imshow(fingers1);
 elseif number_of_fingers1>number_of_fingers2 && number_of_fingers1<6;
     number=number_of_fingers1;
+    subplot(2,2,4);imshow(fingers1);
 elseif number_of_fingers2>number_of_fingers1 && number_of_fingers2<6
     number=number_of_fingers2;
+    subplot(2,2,4);imshow(fingers2);
 elseif number ==10;
 end
